f19399ba6630a0625af974faf99e33e804a73190
doyeon27 <zerohost7375@gmail.com>
2023-12-30 15:49:38 +0900

    서블릿 라이브러리 적용 후 HelloServlet 추가

---
+++ src/main/java/com/sbs/exam/jsp/board/servlet/HelloServlet.java
@@ -0,0 +1,24 @@
+package com.sbs.exam.jsp.board.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet { // HttpServelt 을 상속받음
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("utf-8"); // 들어오는 데이터를 UTF-8 로 해석 한다
+        resp.setCharacterEncoding("utf-8"); // 완성되는 HTML의 인코딩을 UTF-8 로 해석 함
+        resp.setContentType("text/html; charset UTF-8"); // 브라우저에게 우리가 만든 결과물이 UTF-8 이라는걸 의미
+
+        resp.getWriter().append("손흥민");
+
+    }
+}
+
